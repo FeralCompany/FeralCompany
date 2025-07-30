e52b7261562d7950250172364674dc0e5b11e9f0
chuxiaaaa <1612121215@qq.com>
2025-07-31 06:49:22 +0800

    Possible fixes for potential issues

--- FeralCompany/Modules/CurrentRoundData.cs
+++ FeralCompany/Modules/CurrentRoundData.cs
@@ -9,7 +9,7 @@ public sealed class CurrentRoundData : MonoBehaviour
 {
     internal IReadOnlyList<MapPointer> Pointers => _pointers;
 
-    private readonly List<MapPointer> _pointers = [];
+    public static readonly List<MapPointer> _pointers = [];
 
     private void OnExitMoon()
     {
@@ -23,11 +23,6 @@ public sealed class CurrentRoundData : MonoBehaviour
     {
         var obj = GameObject.Find("CatwalkShip") ?? throw new NullReferenceException("CatwalkShip not available??");
         CreatePointer(-1, obj.transform.position, false);
-
-        foreach (var teleport in FindObjectsOfType<EntranceTeleport>())
-            CreatePointer(teleport.entranceId, teleport.entrancePoint.position, teleport.isEntranceToBuilding);
-
-        Feral.Events.InvokePointersCreated(_pointers.ToArray());
     }
 
     private void Awake()
@@ -42,7 +37,7 @@ public sealed class CurrentRoundData : MonoBehaviour
         Feral.Events.OnExitMoon -= OnExitMoon;
     }
 
-    private void CreatePointer(int entranceId, Vector3 destination, bool entrance)
+    public static void CreatePointer(int entranceId, Vector3 destination, bool entrance)
     {
         var obj = Instantiate(Feral.Assets.PrefabMapPointer);
         obj.name = $"Pointer_{entranceId}_{(entrance ? "Entrance" : "Exit")}";
---
+++ FeralCompany/Patches/EntranceTeleportPatch.cs
@@ -0,0 +1,22 @@
+﻿using FeralCompany.Modules;
+
+using HarmonyLib;
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FeralCompany.Patches
+{
+    [HarmonyPatch(typeof(EntranceTeleport))]
+    public class EntranceTeleportPatch
+    {
+        [HarmonyPostfix]
+        [HarmonyPatch("Awake")]
+        public static void Awake(EntranceTeleport __instance)
+        {
+            CurrentRoundData.CreatePointer(__instance.entranceId, __instance.entrancePoint.position, __instance.isEntranceToBuilding);
+            Feral.Events.InvokePointersCreated(CurrentRoundData._pointers.ToArray());
+        }
+    }
+}
